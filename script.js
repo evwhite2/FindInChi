fe972d7446211c583e8b218eb51ca713a4a56655
dgarcia1424 <55024037+dgarcia1424@users.noreply.github.com>
2019-11-01 21:13:49 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,306 @@
+var seatGeekBase= "https://api.seatgeek.com/2";
+var myKey= "&client_id=MTY5MzA1NDd8MTU3MTg3NTc4Mi40NA";
+var byVenues= "/venues?q=";
+var byEvents= "/events?";
+var byPerformers="/performers?";
+var byZip= "/events?postal_code=";
+var userInput="q=";
+var eventLatitude;
+var eventLongitude;
+var eventdate;
+var marker;
+var markers= [];
+var counter= 0;
+
+function myMap()
+            {
+            myCenter=new google.maps.LatLng(41.880650,-87.675140);
+            var mapOptions= {
+            center:myCenter,
+            zoom:12, scrollwheel: false, draggable: false,
+            mapTypeId:google.maps.MapTypeId.ROADMAP
+            };
+            var map=new google.maps.Map(document.getElementById("googleMap"),mapOptions);
+            
+            
+            }
+            function makeTextBox(i){	//this function makes the info box that appears everytime you click on a marker 
+
+              //constructing the string that will go in the text box 
+              var eventsInfo = '<div id="events">'
+                        + '<h1 id="Title">'
+                        + byEvents[i].title
+                        + '</h1>'
+                        + byEvents[i].performers["0"]
+                        + '">'
+                        + byEvents[i].datetime_local
+                       + '</div>';
+              //}
+      
+              return eventsInfo;
+      
+      }
+      function makeMarkers(){	//this function creates the markers that will appear on the map
+
+        //checks if there are already markers on the map
+        if (counter >1){
+        console.log("Counter is more than 1");
+          removeMarkers();
+        }
+      
+        //creates info window
+        var infowindow = new google.maps.InfoWindow();
+      
+      
+        //creating and putting the markers on the map
+          for(var i =0; i < byEvents.length; i++){
+              var myLatLng = {lat: byEvents[i].byVenues.latitude, lng: byEvents[i].byVenues.longitude};
+              marker = new google.maps.Marker({
+                 position: myLatLng,
+                 map: theMap,
+                 title: byEvents[i].title
+              });
+      
+              markers.push(marker); //adding current marker to the marker array
+      
+              google.maps.event.addListener(marker, 'click', (function(marker, i) {
+                return function() {
+                  infowindow.setContent(makeTextBox(i));
+                  infowindow.open(map, marker);
+                }
+             })(marker, i));
+          } // this is where the for loop ends
+      
+          console.log(markers);
+      
+      
+          //sets the map on all of them
+      
+      }
+      
+      function removeMarkers(){
+      
+        console.log("removeMarkers was called");
+      
+           function setMapOnAll(theMap) {
+             for (var i = 0; i < markers.length; i++) {
+                markers[i].setMap(theMap);
+              }
+            }
+      
+        setMapOnAll(null);
+        markers = [];
+      }
+      
+
+
+$("#submit").on("click", callEvents);
+
+function callEvents(){
+
+    $(".headLine").empty();
+        $(".venueName").empty();
+        $(".eventLocation").empty();
+        $(".eventDate").empty();
+        $(".eventImg").attr("src", "");
+        $(".eventLink").empty();
+        $(".sryMsg").empty();
+        $(".sryMsgSub").empty();
+
+    var initialInput= $("#cities").val().trim();
+    userInput=initialInput;
+    console.log("user input: "+ userInput);
+
+    var query= seatGeekBase+byEvents+"q="+userInput+myKey;
+    
+    var settings= 
+        {
+            url: query,
+            method:"GET"
+        };
+
+    var searchSelector= $("#searchSelector").val();
+    
+    console.log(searchSelector, "is my search value");
+
+    if(searchSelector==="0"){
+        
+        console.log("basic");
+        $.ajax(settings).then(basicResults);
+
+    }else if (searchSelector==="1"){
+        // console.log(settings.url);
+        
+        settings.url=seatGeekBase+byPerformers+"q="+userInput+myKey;
+        $.ajax(settings).then(performerResults);
+        
+    }else if(searchSelector==="2"){
+        console.log("search venues");
+        
+        settings.url=seatGeekBase+byVenues+userInput+"&city=chicago"+myKey;
+        // console.log(settings.url);
+        $.ajax(settings).then(venueResults);
+        
+
+    }else if(searchSelector==="3"){
+        console.log("search zips");
+        settings.url= seatGeekBase+byZip+userInput+myKey;
+        console.log(settings.url);
+        $.ajax(settings).then(basicResults);
+
+    };        
+
+
+    function basicResults(response){
+        console.log("we got to basic response function:", response);
+
+        if (response.events.length==0){
+            var sryMsg=$("<h1>").addClass("sryMsg");
+            var sryMsgSub=$("<h3>").addClass("sryMsgSub");
+            sryMsg.text("Sorry we couldn't find any upcoming events based on your search for '"+initialInput+"'.")
+            sryMsgSub.text("Check your spelling or try changing the search parameters.");
+
+            $("#event-wrap").append(sryMsg, sryMsgSub);
+
+        }else{
+
+            for (var i= 0; i<response.events.length; i++) {
+            
+                var newEle0= $("<div>");
+                var newEle1= $("<div>");
+                var newEle2= $("<div>");
+                var newEle3= $("<div>");
+                var newEle4=$("<div>");
+                var newLink= $("<a>");
+                var newImg=$("<img>");
+
+                newEle0.addClass("headLine");
+                newEle1.addClass("venueName");
+                newEle2.addClass("eventLocation");
+                newEle3.addClass("col-md-6");
+                newEle4.addClass("eventDate");
+                newEle3.id = "words" + i;
+                newEle3.addClass("box");
+                newLink.addClass("eventLink");
+
+                newEle0.text(response.events[i].title);
+                newEle1.text(response.events[i].venue.name);
+                newEle2.text(response.events[i].venue.address);
+                newEle4.text(response.events[i].datetime_local);
+                newLink.attr("href", response.events[i].url);
+                newLink.text("Click here to find tickets");
+
+                newEle3.append(newEle0, newEle1, newEle2, newEle4, newLink);
+
+                $("#event-wrap").append(newEle3);
+
+                }
+                if(response.events[i].url==""){
+                    console.log("no link available");
+                }else {
+                    newLink.text("Click here to find tickets");
+                    $(".event-wrap").append(newLink);
+                }
+        }   
+
+    };
+
+    function performerResults(response){
+        console.log("we got to performer response function:", response);
+
+        if(response.performers.length===0
+            // this following tag is not working and I don't know why currently, but if made function could improve the results feedback: 
+            // || response.performers[0].has_upcoming_events==false
+             ){
+
+            var sryMsg=$("<h1>").addClass("sryMsg");
+            var sryMsgSub=$("<h3>").addClass("sryMsgSub");
+            sryMsg.text("Sorry we couldn't find any upcoming events based on your search for '"+initialInput+"'");
+            sryMsgSub.text("Check your spelling or try changing the search parameters.");
+
+            $("#event-wrap").append(sryMsg, sryMsgSub);
+
+        }else{
+            
+            for (var i= 0; i<response.performers.length; i++) {
+            
+            var newEle0= $("<div>");
+            var newEle1= $("<div>");
+            var newEle2= $("<div>");
+            var newEle3= $("<div>");
+            var newEle4=$("<div>");
+            var newLink= $("<a>");
+            var newImg=$("<img>");
+            
+            newEle0.addClass("headLine");
+            newEle1.addClass("venueName");  
+            newEle2.addClass("eventLocation");
+            newEle3.addClass("col-md-6");
+            newEle4.addClass("eventDate");
+            newEle3.id = "words" + i;
+            newEle3.addClass("box");
+            newLink.addClass("eventLink");
+            newLink.text("click here to see tickets");
+            newImg.addClass("eventImg");
+             
+            newEle0.text(response.performers[i].name);
+            newEle1.text(response.performers[i].location);
+            newLink.attr("href", response.performers[i].url);
+            newImg.attr("src", response.performers[i].images.huge);
+
+            newEle3.append(newEle0, newEle1, newEle2, newEle4, newLink, newImg);
+
+            $("#event-wrap").append(newEle3);
+
+            }
+        }
+
+    };
+
+    function venueResults(response){
+        console.log("we got to the venue results function")
+        console.log(response);
+
+        if (response.venues.length===0){
+
+            var sryMsg=$("<h1>").addClass("sryMsg");
+            var sryMsgSub=$("<h3>").addClass("sryMsgSub");
+            sryMsg.text("Sorry we couldn't find any upcoming events based on your search for '"+initialInput+"'");
+            sryMsgSub.text("Check your spelling or try changing the search parameters.");
+            $("#event-wrap").append(sryMsg, sryMsgSub);
+        }else{
+            
+            for(var i=0; i<response.venues.length; i++){
+                
+                var newEle0= $("<div>");
+                var newEle1= $("<div>");
+                var newEle2= $("<div>");
+                var newEle3= $("<div>");
+                var newLink= $("<a>");
+
+                newEle0.addClass("headLine");
+                newEle1.addClass("venueName");
+                newEle2.addClass("eventLocation");
+                newEle3.addClass("col-md-6");
+                newEle3.id = "words" + i;
+                newEle3.addClass("box");
+                newLink.addClass("eventLink");
+
+                newEle0.text(response.venues[i].name);
+                newEle1.text(response.venues[i].address);
+                newEle2.text(response.venues[i].extended_address);
+                newLink.attr("href", response.venues[i].url);
+                newLink.text("Click for more information");
+
+                newEle3.append(newEle0, newEle1, newEle2, newLink);
+
+                $("#event-wrap").append(newEle3);     
+            }
+           
+        }
+
+
+    }
+
+
+}
